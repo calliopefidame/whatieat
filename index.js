b91c5b454f07a24771b19856d217148c1d21017f
Kévin REI-BENTO <kevinreibento@gmail.com>
2020-04-08 10:14:57 +0200

    'modification'

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const app = require('express')();
-var fs = require('fs');
-var privateKey = fs.readFileSync('private.key', 'utf8');
-var certificate = fs.readFileSync('mysubdomain_mydomain_com.crt', 'utf8');
-var credentials = { key: privateKey, cert: certificate };
 const http = require('http').createServer(credentials, app);
 const cors = require('cors');
 
